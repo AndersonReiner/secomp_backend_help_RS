892fc18ada3af92d278c3bc246655ab86bd3528a
AndersonReiner <reiner.academico@gmail.com>
2024-06-01 14:35:05 -0400

    created project

---
+++ src/test/java/com/anderson/secomp_backend_help_RS/SecompBackendHelpRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.secomp_backend_help_RS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecompBackendHelpRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
